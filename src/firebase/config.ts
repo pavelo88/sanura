3b721cd3f48292dcc90abeae38e066312d0ba4fa
Subdirección Nacional de Activos Fijos y Almacén <snafa.dna@funcionjudicial.gob.ec>
2026-03-27 07:17:25 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/firebase/config.ts
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB3Da_l1gcLTyhqvawZsOlfamrt4ygeSRg",
+  authDomain: "nvitality-32876.firebaseapp.com",
+  projectId: "nvitality-32876",
+  storageBucket: "nvitality-32876.firebasestorage.app",
+  messagingSenderId: "779963091768",
+  appId: "1:779963091768:web:dde998520530d6a340cfcd",
+  measurementId: "G-54DECVMKB3"
+};
